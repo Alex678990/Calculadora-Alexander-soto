a95c8eb8be8645b79c97d7aeb69ae37f0cfec7c9
Alex678990 <alexjosovi@gmail.com>
2025-03-13 04:34:05 +0100

    Add files via upload

---
+++ CalculadoraAlexanderSoto/Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtresultado = New TextBox()
+        button7 = New Button()
+        button8 = New Button()
+        button9 = New Button()
+        buttonmenos = New Button()
+        buttonmas = New Button()
+        buttonpor = New Button()
+        button6 = New Button()
+        button5 = New Button()
+        button4 = New Button()
+        buttonentre = New Button()
+        button3 = New Button()
+        button2 = New Button()
+        button1 = New Button()
+        buttonborrar = New Button()
+        buttonigual = New Button()
+        buttonpunto = New Button()
+        button0 = New Button()
+        memoria = New ListBox()
+        SuspendLayout()
+        ' 
+        ' txtresultado
+        ' 
+        txtresultado.Font = New Font("OCR A Extended", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txtresultado.Location = New Point(18, 22)
+        txtresultado.Name = "txtresultado"
+        txtresultado.Size = New Size(162, 42)
+        txtresultado.TabIndex = 0
+        txtresultado.Text = "0"
+        txtresultado.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' button7
+        ' 
+        button7.Font = New Font("OCR A Extended", 12F)
+        button7.Location = New Point(18, 75)
+        button7.Name = "button7"
+        button7.Size = New Size(50, 50)
+        button7.TabIndex = 1
+        button7.Text = "7"
+        button7.UseVisualStyleBackColor = True
+        ' 
+        ' button8
+        ' 
+        button8.Font = New Font("OCR A Extended", 12F)
+        button8.Location = New Point(74, 75)
+        button8.Name = "button8"
+        button8.Size = New Size(50, 50)
+        button8.TabIndex = 2
+        button8.Text = "8"
+        button8.UseVisualStyleBackColor = True
+        ' 
+        ' button9
+        ' 
+        button9.Font = New Font("OCR A Extended", 12F)
+        button9.Location = New Point(130, 75)
+        button9.Name = "button9"
+        button9.Size = New Size(50, 50)
+        button9.TabIndex = 3
+        button9.Text = "9"
+        button9.UseVisualStyleBackColor = True
+        ' 
+        ' buttonmenos
+        ' 
+        buttonmenos.Font = New Font("OCR A Extended", 12F)
+        buttonmenos.Location = New Point(186, 75)
+        buttonmenos.Name = "buttonmenos"
+        buttonmenos.Size = New Size(50, 50)
+        buttonmenos.TabIndex = 4
+        buttonmenos.Text = "-"
+        buttonmenos.UseVisualStyleBackColor = True
+        ' 
+        ' buttonmas
+        ' 
+        buttonmas.Font = New Font("OCR A Extended", 12F)
+        buttonmas.Location = New Point(186, 19)
+        buttonmas.Name = "buttonmas"
+        buttonmas.Size = New Size(50, 50)
+        buttonmas.TabIndex = 5
+        buttonmas.Text = "+"
+        buttonmas.UseVisualStyleBackColor = True
+        ' 
+        ' buttonpor
+        ' 
+        buttonpor.Font = New Font("OCR A Extended", 12F)
+        buttonpor.Location = New Point(186, 131)
+        buttonpor.Name = "buttonpor"
+        buttonpor.Size = New Size(50, 50)
+        buttonpor.TabIndex = 9
+        buttonpor.Text = "*"
+        buttonpor.UseVisualStyleBackColor = True
+        ' 
+        ' button6
+        ' 
+        button6.Font = New Font("OCR A Extended", 12F)
+        button6.Location = New Point(130, 131)
+        button6.Name = "button6"
+        button6.Size = New Size(50, 50)
+        button6.TabIndex = 8
+        button6.Text = "6"
+        button6.UseVisualStyleBackColor = True
+        ' 
+        ' button5
+        ' 
+        button5.Font = New Font("OCR A Extended", 12F)
+        button5.Location = New Point(74, 131)
+        button5.Name = "button5"
+        button5.Size = New Size(50, 50)
+        button5.TabIndex = 7
+        button5.Text = "5"
+        button5.UseVisualStyleBackColor = True
+        ' 
+        ' button4
+        ' 
+        button4.Font = New Font("OCR A Extended", 12F)
+        button4.Location = New Point(18, 131)
+        button4.Name = "button4"
+        button4.Size = New Size(50, 50)
+        button4.TabIndex = 6
+        button4.Text = "4"
+        button4.UseVisualStyleBackColor = True
+        ' 
+        ' buttonentre
+        ' 
+        buttonentre.Font = New Font("OCR A Extended", 12F)
+        buttonentre.Location = New Point(186, 187)
+        buttonentre.Name = "buttonentre"
+        buttonentre.Size = New Size(50, 50)
+        buttonentre.TabIndex = 13
+        buttonentre.Text = "/"
+        buttonentre.UseVisualStyleBackColor = True
+        ' 
+        ' button3
+        ' 
+        button3.Font = New Font("OCR A Extended", 12F)
+        button3.Location = New Point(130, 187)
+        button3.Name = "button3"
+        button3.Size = New Size(50, 50)
+        button3.TabIndex = 12
+        button3.Text = "3"
+        button3.UseVisualStyleBackColor = True
+        ' 
+        ' button2
+        ' 
+        button2.Font = New Font("OCR A Extended", 12F)
+        button2.Location = New Point(74, 187)
+        button2.Name = "button2"
+        button2.Size = New Size(50, 50)
+        button2.TabIndex = 11
+        button2.Text = "2"
+        button2.UseVisualStyleBackColor = True
+        ' 
+        ' button1
+        ' 
+        button1.Font = New Font("OCR A Extended", 12F)
+        button1.Location = New Point(18, 187)
+        button1.Name = "button1"
+        button1.Size = New Size(50, 50)
+        button1.TabIndex = 10
+        button1.Text = "1"
+        button1.UseVisualStyleBackColor = True
+        ' 
+        ' buttonborrar
+        ' 
+        buttonborrar.Font = New Font("OCR A Extended", 12F)
+        buttonborrar.Location = New Point(186, 243)
+        buttonborrar.Name = "buttonborrar"
+        buttonborrar.Size = New Size(50, 50)
+        buttonborrar.TabIndex = 17
+        buttonborrar.Text = "C"
+        buttonborrar.UseVisualStyleBackColor = True
+        ' 
+        ' buttonigual
+        ' 
+        buttonigual.Font = New Font("OCR A Extended", 12F)
+        buttonigual.Location = New Point(130, 243)
+        buttonigual.Name = "buttonigual"
+        buttonigual.Size = New Size(50, 50)
+        buttonigual.TabIndex = 16
+        buttonigual.Text = "="
+        buttonigual.UseVisualStyleBackColor = True
+        ' 
+        ' buttonpunto
+        ' 
+        buttonpunto.Font = New Font("OCR A Extended", 12F)
+        buttonpunto.Location = New Point(74, 243)
+        buttonpunto.Name = "buttonpunto"
+        buttonpunto.Size = New Size(50, 50)
+        buttonpunto.TabIndex = 15
+        buttonpunto.Text = "."
+        buttonpunto.UseVisualStyleBackColor = True
+        ' 
+        ' button0
+        ' 
+        button0.Font = New Font("OCR A Extended", 12F)
+        button0.Location = New Point(18, 243)
+        button0.Name = "button0"
+        button0.Size = New Size(50, 50)
+        button0.TabIndex = 14
+        button0.Text = "0"
+        button0.UseVisualStyleBackColor = True
+        ' 
+        ' memoria
+        ' 
+        memoria.Font = New Font("OCR A Extended", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        memoria.FormattingEnabled = True
+        memoria.ItemHeight = 23
+        memoria.Location = New Point(272, 23)
+        memoria.Name = "memoria"
+        memoria.Size = New Size(247, 257)
+        memoria.TabIndex = 18
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
+        ClientSize = New Size(531, 312)
+        Controls.Add(memoria)
+        Controls.Add(buttonborrar)
+        Controls.Add(buttonigual)
+        Controls.Add(buttonpunto)
+        Controls.Add(button0)
+        Controls.Add(buttonentre)
+        Controls.Add(button3)
+        Controls.Add(button2)
+        Controls.Add(button1)
+        Controls.Add(buttonpor)
+        Controls.Add(button6)
+        Controls.Add(button5)
+        Controls.Add(button4)
+        Controls.Add(buttonmas)
+        Controls.Add(buttonmenos)
+        Controls.Add(button9)
+        Controls.Add(button8)
+        Controls.Add(button7)
+        Controls.Add(txtresultado)
+        Name = "Form1"
+        Text = "Calculadora Alex"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtresultado As TextBox
+    Friend WithEvents button7 As Button
+    Friend WithEvents button8 As Button
+    Friend WithEvents button9 As Button
+    Friend WithEvents buttonmenos As Button
+    Friend WithEvents buttonmas As Button
+    Friend WithEvents buttonpor As Button
+    Friend WithEvents button6 As Button
+    Friend WithEvents button5 As Button
+    Friend WithEvents button4 As Button
+    Friend WithEvents buttonentre As Button
+    Friend WithEvents button3 As Button
+    Friend WithEvents button2 As Button
+    Friend WithEvents button1 As Button
+    Friend WithEvents buttonborrar As Button
+    Friend WithEvents buttonigual As Button
+    Friend WithEvents buttonpunto As Button
+    Friend WithEvents button0 As Button
+    Friend WithEvents memoria As ListBox
+
+End Class
---
+++ CalculadoraAlexanderSoto/Form1.vb
@@ -0,0 +1,134 @@
+﻿Public Class Form1
+
+    Dim operacion As String
+
+    Dim valorresultado As Nullable(Of Double) = Nothing
+
+    Dim valor2 As Nullable(Of Double) = Nothing
+
+    Dim sepresionaoperador As Boolean
+
+    Private Sub button0_Click(sender As Object, e As EventArgs) Handles button0.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "0"
+    End Sub
+
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "1"
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "2"
+    End Sub
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles button3.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "3"
+    End Sub
+
+    Private Sub button4_Click(sender As Object, e As EventArgs) Handles button4.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "4"
+    End Sub
+
+    Private Sub button5_Click(sender As Object, e As EventArgs) Handles button5.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "5"
+    End Sub
+
+    Private Sub button6_Click(sender As Object, e As EventArgs) Handles button6.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "6"
+    End Sub
+
+    Private Sub button7_Click(sender As Object, e As EventArgs) Handles button7.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "7"
+    End Sub
+
+    Private Sub button8_Click(sender As Object, e As EventArgs) Handles button8.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "8"
+    End Sub
+
+    Private Sub button9_Click(sender As Object, e As EventArgs) Handles button9.Click
+        evaluarrestriccionesparaconcatenar()
+        txtresultado.Text &= "9"
+    End Sub
+
+    Private Sub buttonpunto_Click(sender As Object, e As EventArgs) Handles buttonpunto.Click
+        evaluarrestriccionesparaconcatenar()
+        If InStr(txtresultado.Text, ".", CompareMethod.Text) = 0 Then
+            txtresultado.Text &= "."
+        End If
+
+    End Sub
+
+    Private Sub buttonigual_Click(sender As Object, e As EventArgs) Handles buttonigual.Click
+        evaluayhazoperacion()
+        operacion = ""
+        memoria.Items.Add(valorresultado)
+    End Sub
+
+    Private Sub buttonborrar_Click(sender As Object, e As EventArgs) Handles buttonborrar.Click
+        txtresultado.Text = "0"
+        valorresultado = Nothing
+        valor2 = Nothing
+    End Sub
+
+    Private Sub buttonentre_Click(sender As Object, e As EventArgs) Handles buttonentre.Click
+        evaluayhazoperacion()
+        operacion = "/"
+    End Sub
+
+    Private Sub buttonpor_Click(sender As Object, e As EventArgs) Handles buttonpor.Click
+        evaluayhazoperacion()
+        operacion = "*"
+    End Sub
+
+    Private Sub buttonmenos_Click(sender As Object, e As EventArgs) Handles buttonmenos.Click
+        evaluayhazoperacion()
+        operacion = "-"
+    End Sub
+
+    Private Sub buttonmas_Click(sender As Object, e As EventArgs) Handles buttonmas.Click
+        evaluayhazoperacion()
+        operacion = "+"
+    End Sub
+
+    Public Sub evaluayhazoperacion()
+        sepresionaoperador = True
+        valor2 = Val(txtresultado.Text)
+        If valorresultado IsNot Nothing Then
+            Select Case operacion
+                Case "+"
+                    valorresultado += valor2
+                Case "-"
+                    valorresultado -= valor2
+                Case "*"
+                    valorresultado *= valor2
+                Case "/"
+                    valorresultado /= valor2
+            End Select
+            txtresultado.Text = valorresultado
+        Else
+            valorresultado = valor2
+
+        End If
+
+
+    End Sub
+
+    Public Sub evaluarrestriccionesparaconcatenar()
+
+        If sepresionaoperador = True Then
+            txtresultado.Text = ""
+            sepresionaoperador = False
+
+        ElseIf txtresultado.Text = "0" Then
+            txtresultado.Text = ""
+        End If
+    End Sub
+End Class
